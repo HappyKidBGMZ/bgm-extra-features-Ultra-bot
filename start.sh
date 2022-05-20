34caa79887f6a7516ca34ec20ab5e7c3e090d446
HappyKidBGMZ <105188500+HappyKidBGMZ@users.noreply.github.com>
2022-05-20 11:48:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Aadhi000/Ajax.git /Ajax
+  git clone https://github.com/HappyKidBGMZ/bgm-extra-features-Ultra-bot.git /bgm-extra-features-Ultra-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ajax
+  git clone $UPSTREAM_REPO /bgm-extra-features-Ultra-bot
 fi
 cd /Ajax
 pip3 install -U -r requirements.txt
-echo "Starting ᗩᒍᗩ᙭....🔥"
+echo "Starting HAPPY....🔥"
 python3 bot.py
